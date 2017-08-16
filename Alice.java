2b0c890927168aafb4f70e389b7179bcfdcf4474
fundamentaleq <bloodspain@gmail.com>
2017-08-16 21:14:44 +0530

    Msg passing using threads

--- Alice.java
+++ Alice.java
@@ -1,17 +1,112 @@
 import java.io.* ;
 import java.net.* ;
+class listner implements Runnable
+{
+    private Socket clientsocket ;
+    private DataInputStream istream ;
+    public listner(Socket _clientsocket)
+    {
+        try
+        {
+            clientsocket = _clientsocket ;
+            istream = new DataInputStream(clientsocket.getInputStream());
+        }
+        catch (UnknownHostException e)
+        {
+             System.err.println("Don't know about host: hostname") ;
+        } catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to: Alice") ;
+        }
+        new Thread(this,"alice_listener").start() ;
+    }
+    public void run()
+    {
+        String responseLine ;
+        try
+        {
+            while(true)
+            {
+                if((responseLine = istream.readUTF()) != null)
+                {
+                    System.out.println(responseLine);
+                    System.out.flush() ;
+                    if(responseLine == "quit") break ;
+                }
+
+
+            }
+            istream.close() ;
+        }
+        catch (UnknownHostException e)
+        {
+             System.err.println("Don't know about host: hostname") ;
+        } catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to: Alice") ;
+        }
+    }
+}
+class sender implements Runnable
+{
+    private Socket clientsocket ;
+    private DataOutputStream ostream ;
+    private BufferedReader keyRead ;
+    public sender(Socket _clientsocket)
+    {
+        try
+        {
+            clientsocket = _clientsocket ;
+            ostream = new DataOutputStream(clientsocket.getOutputStream());
+            keyRead = new BufferedReader(new InputStreamReader(System.in));
+        }
+        catch (UnknownHostException e)
+        {
+             System.err.println("Don't know about host: hostname") ;
+        } catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to: Alice") ;
+        }
+        new Thread(this,"alice_sender").start() ;
+    }
+    public void run()
+    {
+        String inputline ;
+        try
+        {
+            while(true)
+            {
+                inputline = keyRead.readLine() ;
+                if(inputline == "break") break ;
+                if(inputline != null && !inputline.isEmpty())
+                {
+                    System.out.println(inputline) ;
+                    ostream.writeUTF(inputline);
+                    ostream.flush();
+                }
+                System.out.println("here") ;
+            }
+            ostream.close() ;
+        }
+        catch (UnknownHostException e)
+        {
+             System.err.println("Don't know about host: hostname") ;
+        } catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to: Alice") ;
+        }
+    }
+}
+
 public class Alice
 {
     public static void main(String args[])
     {
         ServerSocket echoServer = null ;
         Socket clientsocket = null ;
-        BufferedReader keyRead = null ;
-        DataInputStream istream = null ;
         try
         {
            echoServer = new ServerSocket(10000);
-           keyRead = new BufferedReader(new InputStreamReader(System.in));// sending to client (pwrite object)
         }
         catch (IOException e)
         {
@@ -19,33 +114,13 @@ public class Alice
         }
         try
         {
-           clientsocket = echoServer.accept() ;
-           istream = new DataInputStream(clientsocket.getInputStream()) ;
-
-           String responseLine, inputline ;
-           while(true)
-           {
-            //   inputline = keyRead.readLine() ;  // keyboard reading
-            //   if(inputline == "break") break ;
-            //   if(inputline != null && !inputline.isEmpty())
-            //   {
-                  //   pwrite.println(inputline);
-                //   pwrite.flush();
-            //   }
-            // responseLine = istream.readUTF() ;
-              if((responseLine = istream.readUTF()) != null)
-              {
-                  System.out.println(responseLine);
-                  System.out.flush() ;
-              }
-              if(responseLine == "aak") break ;
-            }
-           clientsocket.close();
-           istream.close() ;
-
-        }catch (IOException e)
+            clientsocket = echoServer.accept() ;
+        }
+        catch (IOException e)
         {
            System.out.println(e);
         }
+        new listner(clientsocket) ;
+        new sender(clientsocket) ;
     }
 }
--- Bob.java
+++ Bob.java
@@ -1,58 +1,120 @@
 import java.io.*;
 import java.net.* ;
-public class Bob
+class listner implements Runnable
 {
-    public static void main(String args[]) //throws Exception
+    private Socket clientsocket ;
+    private DataInputStream istream ;
+    public listner(Socket _clientsocket)
     {
-        Socket clientsocket = null ;
-        BufferedReader keyRead = null ;
-        DataOutputStream ostream = null ;
-        DataInputStream istream = null ;
         try
         {
-            clientsocket = new Socket("127.0.0.1", 10000) ;// reading from keyboard (keyRead object)
-            keyRead = new BufferedReader(new InputStreamReader(System.in));// sending to client (pwrite object)
-            ostream = new DataOutputStream(clientsocket.getOutputStream());
+            clientsocket = _clientsocket ;
             istream = new DataInputStream(clientsocket.getInputStream());
-        } catch (UnknownHostException e)
+        }
+        catch (UnknownHostException e)
         {
              System.err.println("Don't know about host: hostname") ;
         } catch (IOException e)
         {
             System.err.println("Couldn't get I/O for the connection to: Alice") ;
         }
+        new Thread(this,"bob_listener").start() ;
+    }
+    public void run()
+    {
+        String responseLine ;
         try
         {
-            String responseLine, inputline ;
             while(true)
             {
-               inputline = keyRead.readLine() ;
-               if(inputline == "break") break ;
-               if(inputline != null && !inputline.isEmpty())
-               {
-                   System.out.println(inputline) ;
-                   ostream.writeUTF(inputline);
-                   ostream.flush();
-               }
-            //    if((responseLine = istream.readUTF()) != null) //receive from server
-            //    {
-            //        System.out.println(responseLine);
-            //        System.out.flush() ;
-            //    }
-               System.out.println("here") ;
-             }
-            clientsocket.close();
-            ostream.close() ;
+                if((responseLine = istream.readUTF()) != null)
+                {
+                    System.out.println(responseLine);
+                    System.out.flush() ;
+                    if(responseLine == "quit") break ;
+                }
+
+
+            }
             istream.close() ;
         }
         catch (UnknownHostException e)
         {
-            System.err.println("Trying to connect to unknown host: " + e);
-        }
-         catch (IOException e)
+             System.err.println("Don't know about host: hostname") ;
+        } catch (IOException e)
         {
-                System.err.println("IOException:  " + e);
+            System.err.println("Couldn't get I/O for the connection to: Alice") ;
         }
+    }
+}
+class sender implements Runnable
+{
+    private Socket clientsocket ;
+    private DataOutputStream ostream ;
+    private BufferedReader keyRead ;
+    public sender(Socket _clientsocket)
+    {
+        try
+        {
+            clientsocket = _clientsocket ;
+            ostream = new DataOutputStream(clientsocket.getOutputStream());
+            keyRead = new BufferedReader(new InputStreamReader(System.in));
+        }
+        catch (UnknownHostException e)
+        {
+             System.err.println("Don't know about host: hostname") ;
+        } catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to: Alice") ;
+        }
+        new Thread(this,"bob_sender").start() ;
+    }
+    public void run()
+    {
+        String inputline ;
+        try
+        {
+            while(true)
+            {
+                inputline = keyRead.readLine() ;
+                if(inputline == "break") break ;
+                if(inputline != null && !inputline.isEmpty())
+                {
+                    System.out.println(inputline) ;
+                    ostream.writeUTF(inputline);
+                    ostream.flush();
+                }
+                System.out.println("here") ;
+            }
+            ostream.close() ;
+        }
+        catch (UnknownHostException e)
+        {
+             System.err.println("Don't know about host: hostname") ;
+        } catch (IOException e)
+        {
+            System.err.println("Couldn't get I/O for the connection to: Alice") ;
+        }
+    }
+}
+public class Bob
+{
+    public static void main(String args[]) //throws Exception
+    {
+        Socket clientsocket = null ;
+        try
+        {
+            clientsocket = new Socket("127.0.0.1", 10000) ;
+        } catch (UnknownHostException e)
+        {
+             System.err.println("Don't know about host: hostname") ;
+        }catch (IOException e)
+        {
+           System.err.println("Couldn't get I/O for the connection to: Alice") ;
+        }
+        new listner(clientsocket) ;
+        new sender(clientsocket) ;
+        // wait till both threads are alive and then close the clientport
 
     }
 }
